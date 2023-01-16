3083e35124b03f6a70f4e4b9045c824ddcf53d6a
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2023-01-16 14:34:55 +0200

    Last update

--- ver_22_Cap_2_1/asa-22_70.js
+++ ver_22_Cap_2_1/asa-22_70.js
@@ -369,7 +369,7 @@ webform.validators.asa = function (v, allowOverpass) {
 
     var sumR320 = Decimal(values.CAP3_R330_C1 || 0).plus(values.CAP3_R340_C1 || 0).plus(values.CAP3_R350_C1 || 0);
 
-    if (!(Decimal(values.CAP3_R320_C1 || 0) < sumR320)) {
+    if (!(Decimal(values.CAP3_R320_C1 || 0) >= sumR320)) {
         webform.errors.push({
             'fieldName': 'CAP3_R320_C1',
             'msg': Drupal.t('Cod eroare: 64-038, Cap.3 [r.320 c.1] >= [r.330 c.1]+ [r.340 c.1] + [r.350 c.1]')
